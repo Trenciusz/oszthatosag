7ac1099718db37d80622c218e86e69336854039c
TRENCIUSZ <92546130+Trenciusz@users.noreply.github.com>
2024-10-14 09:56:19 +0200

    Add files via upload

---
+++ 4_1_2.py
@@ -0,0 +1,6 @@
+szam = int(input("Kérlek adj meg egy számot: "))
+
+if szam % 2 == 0:
+    print("A megadott szám páros.")
+else:
+    print("A megadott szám páratlan.")
---
+++ 4_2_3.py
@@ -0,0 +1,10 @@
+szam = int(input("Kérlek adj meg egy számot: "))
+
+if szam % 3 == 0 and szam % 4 == 0:
+    print("A szám osztható 3-mal és 4-gyel is.")
+elif szam % 3 == 0:
+    print("A szám csak 3-mal osztható.")
+elif szam % 4 == 0:
+    print("A szám csak 4-gyel osztható.")
+else:
+    print("A szám sem 3-mal, sem 4-gyel nem osztható.")
